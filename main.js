659f57314042fdd0dd6a0f4026cf24d7218c100c
dengxiaopeng-9527 <dengxiaopeng9527@sina.com>
2024-10-18 00:58:38 +0800

    ui导入

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import App from './App'
-
+import uviewPlus from 'uview-plus'
 // #ifndef VUE3
 import Vue from 'vue'
 import './uni.promisify.adaptor'
@@ -15,6 +15,7 @@ app.$mount()
 import { createSSRApp } from 'vue'
 export function createApp() {
   const app = createSSRApp(App)
+	app.use(uviewPlus)
   return {
     app
   }
